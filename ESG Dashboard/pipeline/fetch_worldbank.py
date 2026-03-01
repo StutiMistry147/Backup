153dac985a21153424d5621c94abd3a41ecfeb84
Stuti Mistry <stutimistry147@gmail.com>
2026-03-01 17:42:52 +0400

    Create fetch_worldbank.py

---
+++ ESG Dashboard/pipeline/fetch_worldbank.py
@@ -0,0 +1,23 @@
+import requests
+import polars as pl
+
+def fetch_co2_data():
+    url = "http://api.worldbank.org/v2/country/USA/indicator/EN.ATM.CO2E.PC?format=json"
+    response = requests.get(url)
+    data = response.json()[1]
+
+    years = []
+    values = []
+
+    for entry in data:
+        if entry["value"] is not None:
+            years.append(int(entry["date"]))
+            values.append(entry["value"])
+
+    df = pl.DataFrame({
+        "Year": years,
+        "CO2_per_Capita": values
+    })
+
+    df.write_csv("data/processed/usa_co2.csv")
+    print("Saved USA CO2 data.")
